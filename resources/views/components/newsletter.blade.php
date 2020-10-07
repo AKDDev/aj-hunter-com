d30047c1094b25dcccf9fe9cfcbd9177a473930f
Alicia Wilkerson <alicia@akddev.net>
2020-10-07 06:16:49 -0500

    working on newsletter

--- resources/views/components/newsletter.blade.php
+++ resources/views/components/newsletter.blade.php
@@ -1,4 +1,8 @@
-<form id="newsletter">
-    <input type="email" id="email" placeholder="Email Address"/>
-    <button type="submit>Join</button>
+<form
+    id="newsletter"
+    method="post"
+    action="https://themindofnox.us15.list-manage.com/subscribe/post?u=fd49bfb92a90f9fe9866a1473&amp;id=d691ad1f28"
+>
+    <input type="email" id="email" name="EMAIL" placeholder="Email Address"/>
+    <button type="submit">Join</button>
 </form>
